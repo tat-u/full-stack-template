a22d1fee323c60cbd2858171c23a5c9a1e04aeef
tat-u <uetatsu1005@gmail.com>
2026-02-02 21:18:46 +0900

    chore: ローカル起動を HTTPS に

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,16 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
 
   modules: [
-    '@nuxt/test-utils',
-    '@nuxt/image',
-    '@nuxt/fonts',
-    '@nuxt/content',
-    '@nuxt/a11y',
-    '@nuxt/hints',
-    '@nuxt/icon',
-    '@nuxt/eslint'
-  ]
-})
+    "@nuxt/test-utils",
+    "@nuxt/image",
+    "@nuxt/fonts",
+    "@nuxt/content",
+    "@nuxt/a11y",
+    "@nuxt/hints",
+    "@nuxt/icon",
+    "@nuxt/eslint",
+  ],
+
+  devServer: {
+    https: {
+      key: "./server.key",
+      cert: "./server.crt",
+    },
+  },
+});
